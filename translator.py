b2c68db477d39e8b8204a60fa4aaec34324ed51a
jsmit124 <jsmit124@my.westga.edu>
2020-12-10 11:34:21 -0500

    Initial commit

---
+++ translator.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Translator
+
+@author Justin Smith
+@version 1.0
+"""
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from google_trans_new import google_translator
+import re
+
+Tk().withdraw() 
+filename = askopenfilename() 
+print(filename)
+
+baseFile = open(filename, 'r')
+newFile = open('renameMe.xlf', 'w')
+
+lines = baseFile.readlines()
+
+temp = ''
+translator = google_translator()
+
+x = 0
+
+while x < (len(lines)):
+    line = lines[x]
+    phraseToTranslate = ''
+    
+    if '<source>' in line:
+        phraseToTranslate = line[line.find('<source>') + 8 : line.rfind('</source>')]
+        copyPhrase = line
+        
+        while '</source>' not in copyPhrase:
+            x = x + 1
+            phraseToTranslate = phraseToTranslate + lines[x]
+            copyPhrase = copyPhrase + lines[x]
+        
+        if '<' in phraseToTranslate:
+            phraseToTranslate = re.sub(r'<.+?>', '', phraseToTranslate)
+        
+        result = ''
+        if len(phraseToTranslate.strip()) > 0:
+            result = translator.translate(phraseToTranslate, lang_src='en', lang_tgt='es')
+        
+        if isinstance(result, list):
+            result = result[0]
+            
+        if '  ' in result:
+            result = re.sub('  ', '\n\t', result)
+            
+        if (len(result.strip()) > 0):
+            newFile.write(copyPhrase)
+            
+            targetPhrase = copyPhrase.replace('source', 'target')
+            targetPhrase = targetPhrase.replace(phraseToTranslate.strip(), result.strip())
+            
+            newFile.write(targetPhrase)
+        else:
+            newFile.write(copyPhrase)
+            newFile.write('\t\t<target></target>// TODO\n')
+        x = x + 1
+    else:
+        newFile.writelines(line)
+        x = x + 1
+        
+baseFile.close()
+newFile.close()
