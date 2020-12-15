b59200228803c8874f7cfa572b79236cb8306bc4
jsmit124 <jsmit124@my.westga.edu>
2020-12-15 08:38:41 -0500

    Added correct file naming convention and bit of cleanup

--- translator.py
+++ translator.py
@@ -4,38 +4,68 @@ Translator
 
 @author Justin Smith
 @version 1.0
+
 """
 
+
+'''
+- - - - - - - - - - - - - Imports - - - - - - - - - - - - -
+'''
+import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from google_trans_new import google_translator
 import re
 
+
+'''
+- - - - - - - - - - - - - Global Variables - - - - - - - - - - - - -
+'''
+openingSourceTag = '<source>'
+closingSourceTag = '</source>'
+
+
+'''
+- - - - - - - - - - - - - User Input - - - - - - - - - - - - -
+'''
 Tk().withdraw() 
 filename = askopenfilename() 
-print(filename)
+print("File selected: " + filename + "\n")
 
+print("Please see our Wiki page for the language codes you may enter (https://github.com/jsmit124/i18n-xlf-file-translator)\n")
 targetLanguage = input("Please enter the language code for the target language (ex. 'en', 'es', 'fr', etc): ")
 
+
+'''
+- - - - - - - - - - - - - IO - - - - - - - - - - - - -
+'''
+base = os.path.basename(filename)
+splitBase = os.path.splitext(base)[0]
+translatedFileName = re.sub(splitBase, splitBase + "." + targetLanguage, filename)
+
 baseFile = open(filename, 'r')
-newFile = open('renameMe.xlf', 'w')
+newFile = open(translatedFileName, 'w')
 
 lines = baseFile.readlines()
 
 temp = ''
 translator = google_translator()
 
+
+'''
+- - - - - - - - - - - - - Main - - - - - - - - - - - - -
+'''
 x = 0
 
 while x < (len(lines)):
     line = lines[x]
     phraseToTranslate = ''
     
-    if '<source>' in line:
-        phraseToTranslate = line[line.find('<source>') + 8 : line.rfind('</source>')]
+    if openingSourceTag in line:
+        phraseToTranslate = line[line.find(openingSourceTag) + 8 : line.rfind(closingSourceTag)]
         copyPhrase = line
         
-        while '</source>' not in copyPhrase:
+        while closingSourceTag not in copyPhrase:
             x = x + 1
             phraseToTranslate = phraseToTranslate + lines[x]
             copyPhrase = copyPhrase + lines[x]
@@ -68,5 +98,9 @@ while x < (len(lines)):
         newFile.writelines(line)
         x = x + 1
         
+        
+'''
+- - - - - - - - - - - - - Cleanup - - - - - - - - - - - - -
+'''
 baseFile.close()
 newFile.close()
